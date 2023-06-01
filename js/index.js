4e000833c9e5d3806ee44a89478797a2e526cae5
ZahraCassimjee <231131@virtualwindow.co.za>
2023-06-01 23:12:28 +0200

    classwork

--- js/index.js
+++ js/index.js
@@ -44,6 +44,90 @@ makePizza = () =>{
 
     console.log(pizzaOrder)
 
+document.getElementById("realTimeCost").innerHTML = "R0.00"
     document.getElementById("pizzaForm").reset();
 
 }
+
+realTimeCost = () => {
+
+realTimeValue = 0;
+
+let size = document.getElementById("size").value;
+
+    if(size === "Small"){
+        realTimeValue = realTimeValue + 20;
+    } else if(size === "Medium"){
+        realTimeValue = realTimeValue + 40;
+    } else if(size === "Large"){
+        realTimeValue = realTimeValue + 60;
+    }
+
+
+
+    let baseOption = document.getElementsByName("baseRadio");
+ 
+    for(let i = 0; i < baseOption.length; i++){
+        if(baseOption[i].checked){
+            realTimeValue = realTimeValue + +baseOption[i].dataset.cost
+        }
+    }
+
+    let toppingOptions = document.getElementsByName("toppings");
+   
+    for(let i = 0; i < toppingOptions.length; i++){
+        if(toppingOptions[i].checked){
+            realTimeValue = realTimeValue + +toppingOptions[i].dataset.cost
+        }
+    }
+
+
+    
+
+    document.getElementById("realTimeCost").innerHTML = "R" + realTimeValue + ".00"
+
+   
+
+}
+
+displayOrder = () => {
+
+let area = document.getElementById("orders");
+let total = document.getElementById("orderTotal");
+
+area.innerHTML = "";
+
+let overallTotal = 0;
+
+for( let i = 0; i < pizzaOrder.length; i++){
+
+    let name = pizzaOrder[i].pizzaName
+    let size = pizzaOrder[i].pizzaSize
+    let base= pizzaOrder[i].pizzaBase
+    let toppings = pizzaOrder[i].pizzaToppings
+    let cost = pizzaOrder[i].pizzaPrice
+
+overallTotal += cost;
+
+area.innerHTML += `
+
+<div class="card">
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text"><strong>base:</strong>  ${base}</p>
+            <p class="card-text"><strong>Size:</strong>  ${size}</p>
+            <p class="card-text"><strong>Toppings:</strong> ${toppings.join(", ")}</p>
+            <p class="card-text"><strong>Cost:</strong>  ${cost}.00</p>
+        </div>
+</div> `
+
+total.innerHTML = "R" + overallTotal + ".00";
+
+
+}
+
+}
+
+
+
+
